a45850e40dac119af6e4cb79a3af9854ece4e943
denyszamiatin <denys.zamiatin@gmail.com>
2019-11-26 21:05:29 +0200

    tests

--- test_all.py
+++ test_all.py
@@ -1,13 +1,13 @@
 import all
 
 
-def test_empty():
+def test_true():
     assert all.all([True, True]) == True
 
 
-def test_odd():
+def test_false():
     assert all.all([True, False]) == False
 
 
-def test_string():
+def test_one_item():
     assert all.all([True]) == True
