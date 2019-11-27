896becc30e37cdaca82ae480c1dfb610ab6ad8ec
YuPo13 <yuliya.povzlo@gmail.com>
2019-11-27 18:49:18 +0200

    Lambda is tricky but simplifiable-if-statements are even trickier

--- all.py
+++ all.py
@@ -5,4 +5,4 @@ import functools
 
 def new_all(initial_list):
     """docstring"""
-    return functools.reduce(lambda x, y: (x and y) is True, initial_list)
+    return functools.reduce(lambda x, y: x and y, initial_list)
