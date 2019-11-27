50685d58752dfb699d306c7c4047ed05bb4eda4a
DukhDmytro <cowboybebop4991@gmail.com>
2019-11-27 11:23:52 +0200

    1st commit

--- all.py
+++ all.py
@@ -1,6 +1,10 @@
-from typing import Callable, Iterable, Any, List
-import functools
+"""import"""
+from typing import Iterable
 
 
-def all(l: Iterable[bool]) -> bool:
-    return 0
+def all_(lst: Iterable[bool]) -> bool:
+    """all"""
+    for ele in lst:
+        if not ele:
+            return False
+    return True
