82c92d8a1fa2aa5ba4546369a144fb01eebb1b43
alexander <earthhuman41@gmail.com>
2019-11-28 19:09:42 +0200

    fixed solution

--- all.py
+++ all.py
@@ -10,4 +10,4 @@ def all_(items: Iterable[bool]) -> bool:
     :param items: list
     :return: is all items true
     """
-    return functools.reduce(lambda _, y: bool(y), items)
+    return functools.reduce(lambda x, y: x and y, items)
